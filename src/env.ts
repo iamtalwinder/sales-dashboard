fc0a214069ef1acd00314c20db311e8595641675
Talwinder Singh <singhtalwinder790@gmail.com>
2024-08-02 21:33:40 +0530

    feat: add env var & docker componse

--- src/env.ts
+++ src/env.ts
@@ -9,11 +9,14 @@ export enum AppEnv {
 export const env = createEnv({
   server: {
     MONGODB_URI: z.string().min(1),
-    NODE_ENV: z.nativeEnum(AppEnv)
+    NODE_ENV: z.nativeEnum(AppEnv),
   },
-  client: {},
+  client: {
+    NEXT_PUBLIC_API_BASE_URL: z.string().min(1)
+  },
   runtimeEnv: {
     NODE_ENV: process.env.NODE_ENV,
-    MONGODB_URI: process.env.MONGODB_URI
+    MONGODB_URI: process.env.MONGODB_URI,
+    NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL
   },
 });
--- src/facade/sales.ts
+++ src/facade/sales.ts
@@ -1,7 +1,10 @@
+import { env } from '@dashboard/env';
 import { ISalesData } from '@dashboard/modules/sales';
 
+const SALES_API = `${env.NEXT_PUBLIC_API_BASE_URL}/api/sales`;
+
 export const fetchSalesData = async (start: string, end: string): Promise<ISalesData[]> => {
-  const response = await fetch(`/api/sales?start=${start}&end=${end}`);
+  const response = await fetch(`${SALES_API}?start=${start}&end=${end}`);
   const data = await response.json();
   return data;
 };
